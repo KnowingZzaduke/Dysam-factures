b622550a8d4ac12f32352cccecdbfe8c0e54e94f
KnowingZzaduke <josex.developer@gmail.com>
2024-02-24 23:45:06 -0500

    Configurando las formulas

--- src/components/contabilidad/TablaEnviarFacturas.tsx
+++ src/components/contabilidad/TablaEnviarFacturas.tsx
@@ -1,35 +1,54 @@
 import { HotTable, HotColumn } from "@handsontable/react";
 import { registerAllModules } from "handsontable/registry";
 import { registerLanguageDictionary, esMX } from "handsontable/i18n";
+import HyperFormula from "hyperformula";
 import "handsontable/dist/handsontable.full.css";
 import { SecondCells, infoSecondCells } from "../../data/dataCells";
 registerAllModules();
 registerLanguageDictionary(esMX);
 import { useEffect } from "react";
-
 function TablaEnviarFacturas() {
+
+  useEffect(() => {
+    console.log(infoSecondCells);
+    formulasPlugin.engine.changeNamedExpression('ADDITIONAL_COST', );
+  }, [infoSecondCells]);
+
   return (
     <div className="h-screen flex justify-center">
       <HotTable
         language={esMX.languageCode}
         licenseKey="non-commercial-and-evaluation"
-        data={infoSecondCells.detalleMod}
-        colHeaders={true}
+        data={infoSecondCells}
+        colHeaders={[
+          "DETALLE DE M.O.D.",
+          "BASICO HORA",
+          "HOR-EST",
+          "TOTAL TIEMPO/ HORAS",
+          "VR DIA ",
+          "DIA ESTIMADOS",
+          "TOTAL DIAS",
+        ]}
         rowHeaders={true}
         columnSorting={true}
         mergeCells={true}
         contextMenu={["row_above", "row_below"]}
         readOnly={false}
+        autoWrapCol={true}
+        autoWrapRow={true}
+        manualRowMove={true}
+        formulas={{
+          engine: HyperFormula,
+        }}
         className="-z-0"
       >
-        <HotColumn data="name" title="Detalle de mod" />
-        <HotColumn title="Básico ahora" />
-        <HotColumn title="Hor-Est" />
-        <HotColumn title="Total tiempo/horas" />
-        <HotColumn title="Vr día" />
-        <HotColumn title="Días estimados" />
-        <HotColumn title="Total días" />
-        <HotColumn title="Mano obra directa" />
+        <HotColumn data={1} />
+        <HotColumn data={2} />
+        <HotColumn data={3} />
+        <HotColumn data={4} />
+        <HotColumn data={5} />
+        <HotColumn data={6} />
+        <HotColumn data={7} />
       </HotTable>
     </div>
   );
